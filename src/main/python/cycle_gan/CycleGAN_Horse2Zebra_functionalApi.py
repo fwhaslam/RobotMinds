49a5102f57f96b84dfb0894bc175ef0cf1d08468
Frederick W Haslam <fwhaslam@gmail.com>
2022-08-25 22:50:45 -0700

    Importing unet_generator from pix2pix source ( instead of example on web-page )

--- src/main/python/cycle_gan/CycleGAN_Horse2Zebra_functionalApi.py
+++ src/main/python/cycle_gan/CycleGAN_Horse2Zebra_functionalApi.py
@@ -35,8 +35,14 @@ dataset, metadata = tfds.load('cycle_gan/horse2zebra',
 train_horses, train_zebras = dataset['trainA'], dataset['trainB']
 test_horses, test_zebras = dataset['testA'], dataset['testB']
 
+###########################################################################################################
+#       Constant Parameters
+
+
 BUFFER_SIZE = 1000
 BATCH_SIZE = 1
+EPOCHS = 40
+checkpoint_path = "zebra_fapi_ckpt/train"
 
 train_horses = train_horses.cache().map(
     preprocess_image_train, num_parallel_calls=AUTOTUNE).shuffle(
@@ -57,14 +63,53 @@ test_zebras = test_zebras.map(
 # select one horse/zebra for demonstrating progress :: Display alongside generator images
 sample_horse = next(iter(train_horses))
 sample_zebra = next(iter(train_zebras))
-horse_loop = iter(MyLoop(train_horses))
+horse_loop = iter(RepeatLoop(train_horses))
 
 OUTPUT_CHANNELS = 3
 
 ########################################################################################################################
-#   Functional Api version of UNet generator
+#   Functional Api version of UNet generator from tensorflow_examples.models.pix2pix
+#       see: https://github.com/tensorflow/examples/blob/master/tensorflow_examples/models/pix2pix/pix2pix.py
+#
 
-def downsample(filters, size, apply_batchnorm=True):
+class InstanceNormalization(tf.keras.layers.Layer):
+    """Instance Normalization Layer (https://arxiv.org/abs/1607.08022)."""
+
+    def __init__(self, epsilon=1e-5):
+        super(InstanceNormalization, self).__init__()
+        self.epsilon = epsilon
+
+    def build(self, input_shape):
+        self.scale = self.add_weight(
+            name='scale',
+            shape=input_shape[-1:],
+            initializer=tf.random_normal_initializer(1., 0.02),
+            trainable=True)
+
+        self.offset = self.add_weight(
+            name='offset',
+            shape=input_shape[-1:],
+            initializer='zeros',
+            trainable=True)
+
+    def call(self, x):
+        mean, variance = tf.nn.moments(x, axes=[1, 2], keepdims=True)
+        inv = tf.math.rsqrt(variance + self.epsilon)
+        normalized = (x - mean) * inv
+        return self.scale * normalized + self.offset
+
+
+def downsample(filters, size, norm_type='batchnorm', apply_norm=True):
+    """Downsamples an input.
+    Conv2D => Batchnorm => LeakyRelu
+    Args:
+      filters: number of filters
+      size: filter size
+      norm_type: Normalization type; either 'batchnorm' or 'instancenorm'.
+      apply_norm: If True, adds the batchnorm layer
+    Returns:
+      Downsample Sequential Model
+    """
     initializer = tf.random_normal_initializer(0., 0.02)
 
     result = tf.keras.Sequential()
@@ -72,14 +117,29 @@ def downsample(filters, size, apply_batchnorm=True):
         tf.keras.layers.Conv2D(filters, size, strides=2, padding='same',
                                kernel_initializer=initializer, use_bias=False))
 
-    if apply_batchnorm:
-        result.add(tf.keras.layers.BatchNormalization())
+    if apply_norm:
+        if norm_type.lower() == 'batchnorm':
+            result.add(tf.keras.layers.BatchNormalization())
+        elif norm_type.lower() == 'instancenorm':
+            result.add(InstanceNormalization())
 
     result.add(tf.keras.layers.LeakyReLU())
 
     return result
 
-def upsample(filters, size, apply_dropout=False):
+
+def upsample(filters, size, norm_type='batchnorm', apply_dropout=False):
+    """Upsamples an input.
+    Conv2DTranspose => Batchnorm => Dropout => Relu
+    Args:
+      filters: number of filters
+      size: filter size
+      norm_type: Normalization type; either 'batchnorm' or 'instancenorm'.
+      apply_dropout: If True, adds the dropout layer
+    Returns:
+      Upsample Sequential Model
+    """
+
     initializer = tf.random_normal_initializer(0., 0.02)
 
     result = tf.keras.Sequential()
@@ -89,7 +149,10 @@ def upsample(filters, size, apply_dropout=False):
                                         kernel_initializer=initializer,
                                         use_bias=False))
 
-    result.add(tf.keras.layers.BatchNormalization())
+    if norm_type.lower() == 'batchnorm':
+        result.add(tf.keras.layers.BatchNormalization())
+    elif norm_type.lower() == 'instancenorm':
+        result.add(InstanceNormalization())
 
     if apply_dropout:
         result.add(tf.keras.layers.Dropout(0.5))
@@ -98,37 +161,46 @@ def upsample(filters, size, apply_dropout=False):
 
     return result
 
-def Generator():
-    inputs = tf.keras.layers.Input(shape=[256, 256, 3])
+
+def unet_generator(output_channels, norm_type='batchnorm'):
+    """Modified u-net generator model (https://arxiv.org/abs/1611.07004).
+    Args:
+      output_channels: Output channels
+      norm_type: Type of normalization. Either 'batchnorm' or 'instancenorm'.
+    Returns:
+      Generator model
+    """
 
     down_stack = [
-        downsample(64, 4, apply_batchnorm=False),  # (batch_size, 128, 128, 64)
-        downsample(128, 4),  # (batch_size, 64, 64, 128)
-        downsample(256, 4),  # (batch_size, 32, 32, 256)
-        downsample(512, 4),  # (batch_size, 16, 16, 512)
-        downsample(512, 4),  # (batch_size, 8, 8, 512)
-        downsample(512, 4),  # (batch_size, 4, 4, 512)
-        downsample(512, 4),  # (batch_size, 2, 2, 512)
-        downsample(512, 4),  # (batch_size, 1, 1, 512)
+        downsample(64, 4, norm_type, apply_norm=False),  # (bs, 128, 128, 64)
+        downsample(128, 4, norm_type),  # (bs, 64, 64, 128)
+        downsample(256, 4, norm_type),  # (bs, 32, 32, 256)
+        downsample(512, 4, norm_type),  # (bs, 16, 16, 512)
+        downsample(512, 4, norm_type),  # (bs, 8, 8, 512)
+        downsample(512, 4, norm_type),  # (bs, 4, 4, 512)
+        downsample(512, 4, norm_type),  # (bs, 2, 2, 512)
+        downsample(512, 4, norm_type),  # (bs, 1, 1, 512)
     ]
 
     up_stack = [
-        upsample(512, 4, apply_dropout=True),  # (batch_size, 2, 2, 1024)
-        upsample(512, 4, apply_dropout=True),  # (batch_size, 4, 4, 1024)
-        upsample(512, 4, apply_dropout=True),  # (batch_size, 8, 8, 1024)
-        upsample(512, 4),  # (batch_size, 16, 16, 1024)
-        upsample(256, 4),  # (batch_size, 32, 32, 512)
-        upsample(128, 4),  # (batch_size, 64, 64, 256)
-        upsample(64, 4),  # (batch_size, 128, 128, 128)
+        upsample(512, 4, norm_type, apply_dropout=True),  # (bs, 2, 2, 1024)
+        upsample(512, 4, norm_type, apply_dropout=True),  # (bs, 4, 4, 1024)
+        upsample(512, 4, norm_type, apply_dropout=True),  # (bs, 8, 8, 1024)
+        upsample(512, 4, norm_type),  # (bs, 16, 16, 1024)
+        upsample(256, 4, norm_type),  # (bs, 32, 32, 512)
+        upsample(128, 4, norm_type),  # (bs, 64, 64, 256)
+        upsample(64, 4, norm_type),  # (bs, 128, 128, 128)
     ]
 
     initializer = tf.random_normal_initializer(0., 0.02)
-    last = tf.keras.layers.Conv2DTranspose(OUTPUT_CHANNELS, 4,
-                                           strides=2,
-                                           padding='same',
-                                           kernel_initializer=initializer,
-                                           activation='tanh')  # (batch_size, 256, 256, 3)
+    last = tf.keras.layers.Conv2DTranspose(
+        output_channels, 4, strides=2,
+        padding='same', kernel_initializer=initializer,
+        activation='tanh')  # (bs, 256, 256, 3)
 
+    concat = tf.keras.layers.Concatenate()
+
+    inputs = tf.keras.layers.Input(shape=[None, None, 3])
     x = inputs
 
     # Downsampling through the model
@@ -142,45 +214,64 @@ def Generator():
     # Upsampling and establishing the skip connections
     for up, skip in zip(up_stack, skips):
         x = up(x)
-        x = tf.keras.layers.Concatenate()([x, skip])
+        x = concat([x, skip])
+
     x = last(x)
 
     return tf.keras.Model(inputs=inputs, outputs=x)
 
-def Discriminator():
+
+def discriminator(norm_type='batchnorm', target=True):
+    """PatchGan discriminator model (https://arxiv.org/abs/1611.07004).
+    Args:
+      norm_type: Type of normalization. Either 'batchnorm' or 'instancenorm'.
+      target: Bool, indicating whether target image is an input or not.
+    Returns:
+      Discriminator model
+    """
+
     initializer = tf.random_normal_initializer(0., 0.02)
 
-    inp = tf.keras.layers.Input(shape=[256, 256, 3], name='input_image')
-    tar = tf.keras.layers.Input(shape=[256, 256, 3], name='target_image')
+    inp = tf.keras.layers.Input(shape=[None, None, 3], name='input_image')
+    x = inp
 
-    x = tf.keras.layers.concatenate([inp, tar])  # (batch_size, 256, 256, channels*2)
+    if target:
+        tar = tf.keras.layers.Input(shape=[None, None, 3], name='target_image')
+        x = tf.keras.layers.concatenate([inp, tar])  # (bs, 256, 256, channels*2)
 
-    down1 = downsample(64, 4, False)(x)  # (batch_size, 128, 128, 64)
-    down2 = downsample(128, 4)(down1)  # (batch_size, 64, 64, 128)
-    down3 = downsample(256, 4)(down2)  # (batch_size, 32, 32, 256)
+    down1 = downsample(64, 4, norm_type, False)(x)  # (bs, 128, 128, 64)
+    down2 = downsample(128, 4, norm_type)(down1)  # (bs, 64, 64, 128)
+    down3 = downsample(256, 4, norm_type)(down2)  # (bs, 32, 32, 256)
 
-    zero_pad1 = tf.keras.layers.ZeroPadding2D()(down3)  # (batch_size, 34, 34, 256)
-    conv = tf.keras.layers.Conv2D(512, 4, strides=1,
-                                  kernel_initializer=initializer,
-                                  use_bias=False)(zero_pad1)  # (batch_size, 31, 31, 512)
+    zero_pad1 = tf.keras.layers.ZeroPadding2D()(down3)  # (bs, 34, 34, 256)
+    conv = tf.keras.layers.Conv2D(
+        512, 4, strides=1, kernel_initializer=initializer,
+        use_bias=False)(zero_pad1)  # (bs, 31, 31, 512)
 
-    batchnorm1 = tf.keras.layers.BatchNormalization()(conv)
+    if norm_type.lower() == 'batchnorm':
+        norm1 = tf.keras.layers.BatchNormalization()(conv)
+    elif norm_type.lower() == 'instancenorm':
+        norm1 = InstanceNormalization()(conv)
 
-    leaky_relu = tf.keras.layers.LeakyReLU()(batchnorm1)
+    leaky_relu = tf.keras.layers.LeakyReLU()(norm1)
 
-    zero_pad2 = tf.keras.layers.ZeroPadding2D()(leaky_relu)  # (batch_size, 33, 33, 512)
+    zero_pad2 = tf.keras.layers.ZeroPadding2D()(leaky_relu)  # (bs, 33, 33, 512)
 
-    last = tf.keras.layers.Conv2D(1, 4, strides=1,
-                                  kernel_initializer=initializer)(zero_pad2)  # (batch_size, 30, 30, 1)
+    last = tf.keras.layers.Conv2D(
+        1, 4, strides=1,
+        kernel_initializer=initializer)(zero_pad2)  # (bs, 30, 30, 1)
 
-    return tf.keras.Model(inputs=[inp, tar], outputs=last)
+    if target:
+        return tf.keras.Model(inputs=[inp, tar], outputs=last)
+    else:
+        return tf.keras.Model(inputs=inp, outputs=last)
 
 
-generator_g = Generator()(OUTPUT_CHANNELS, norm_type='instancenorm')
-generator_f = Generator()(OUTPUT_CHANNELS, norm_type='instancenorm')
+generator_g = unet_generator(OUTPUT_CHANNELS, norm_type='instancenorm')
+generator_f = unet_generator(OUTPUT_CHANNELS, norm_type='instancenorm')
 
-discriminator_x = Discriminator()(norm_type='instancenorm', target=False)
-discriminator_y = Discriminator()(norm_type='instancenorm', target=False)
+discriminator_x = discriminator(norm_type='instancenorm', target=False)
+discriminator_y = discriminator(norm_type='instancenorm', target=False)
 
 ########################################################################################################################
 
@@ -234,8 +325,6 @@ generator_f_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
 discriminator_x_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
 discriminator_y_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
 
-checkpoint_path = "zebra_ckpt/train"
-
 ckpt = tf.train.Checkpoint(generator_g=generator_g,
                            generator_f=generator_f,
                            discriminator_x=discriminator_x,
@@ -252,7 +341,6 @@ if ckpt_manager.latest_checkpoint:
     ckpt.restore(ckpt_manager.latest_checkpoint)
     print ('Latest checkpoint restored!!')
 
-EPOCHS = 40
 
 def generate_images(model, test_input):
     prediction = model(test_input)
