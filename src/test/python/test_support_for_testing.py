ef03243d7cbaa02a3c2ef7e28bbd1aab7a174333
Frederick W Haslam <fwhaslam@gmail.com>
2022-10-01 19:01:49 -0700

    Fixing support_for_testing tests.

--- src/test/python/test_support_for_testing.py
+++ src/test/python/test_support_for_testing.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 import unittest
 
 # module under test
-from support_for_testing import *
+import support_for_testing as sft
 
 
 class test_support_for_testing(unittest.TestCase):
@@ -12,31 +12,25 @@ class test_support_for_testing(unittest.TestCase):
     def test__loss_has_gradient__true(self):
         def loss_with_subtraction(input_x, output_x):
             return output_x - input_x
-        result = loss_has_gradient( loss_with_subtraction )
+        result = sft.loss_has_gradient( loss_with_subtraction, arg_count=2 )
         self.assertTrue( result )
 
     def test__loss_has_gradient__false(self):
         def loss_with_argmax(input_x, output_x):
             return tf.argmax( output_x - input_x )
-        result = loss_has_gradient( loss_with_argmax )
+        result = sft.loss_has_gradient( loss_with_argmax, arg_count=2 )
         self.assertFalse( result )
 
-    def test__loss_has_gradient__lamdba(self):
+    def test__loss_has_gradient__single_arg(self):
         def single_argument_loss(output_x):
             return output_x
-        result = loss_has_gradient( lambda x,y: single_argument_loss(x-y) )
+        result = sft.loss_has_gradient( single_argument_loss )
         self.assertTrue( result )
 
     def test__loss_has_gradient__shape(self):
         def loss_with_subtraction(input_x, output_x):
             return output_x - input_x
-        result = loss_has_gradient( loss_with_subtraction, (2,2) )
-        self.assertTrue( result )
-
-    def test__loss_has_gradient__lamdba_shape(self):
-        def single_argument_loss(output_x):
-            return output_x
-        result = loss_has_gradient( lambda x,y: single_argument_loss(x-y), (2,2) )
+        result = sft.loss_has_gradient( loss_with_subtraction, arg_count=2, output_shape=(2,2) )
         self.assertTrue( result )
 
 if __name__ == '__main__':
