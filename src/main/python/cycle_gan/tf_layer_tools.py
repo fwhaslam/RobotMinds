18dae38a04a9d1eb91b4982db2a2c8ce3417ec8b
Frederick W Haslam <fwhaslam@gmail.com>
2022-10-01 19:48:59 -0700

    Moving softargmax to tf_tensor_tools.py

--- src/main/python/cycle_gan/tf_layer_tools.py
+++ src/main/python/cycle_gan/tf_layer_tools.py
@@ -92,34 +92,3 @@ class SimpleImageCrop(tf.keras.layers.Layer):
 def crop_layer( y, x, tall, wide ):
     r"""Simplified cropping layer."""
     return SimpleImageCrop(y, x, tall, wide)
-
-@tf.function
-def softargmax( alpha:tf.Tensor, beta=64. ):
-    r"""Softargmax implementation, applied to last dimension of tensor.
-    Produces results similar to argmax, but is differentiable.
-
-    :param alpha: a tensor matrix containing values we want to reduce
-    :param beta: exponentiation factor which causes softmax to produce values closer to zero or one.
-    :return: matrix which reduces the last dimension of alpha to a softargmax value.
-
-    see: https://medium.com/@nicolas.ugrinovic.k/soft-argmax-soft-argmin-and-other-soft-stuff-7f94e6120dff
-    see: https://www.tutorialexample.com/understand-softargmax-an-improvement-of-argmax-tensorflow-tutorial/
-    """
-
-    # increase array values so exponentiation of softmax is more extreme
-    betalpha = beta * alpha
-    # tf.print('betalpha=',betalpha)
-    # extreme exponentiation creates numbers close to zero or one ( ~one for biggest entry)
-    smax = tf.nn.softmax( betalpha )
-    # tf.print('sm=',smax)
-    # size of last dimension as a range of integers [ 0, 1, ... N ]
-    pos = range( alpha.shape[-1] )
-    # tf.print('pos=',pos)
-    # array has ~zero for most entries, and ( ~one * index ) for maximum entry
-    smpos = smax * pos
-    # tf.print('smpos=',smpos)
-    # sum of all values in array will approximate ( ~one * index )
-    softargmax = tf.reduce_sum( smpos, axis=-1)
-    # tf.print('softargmax=',softargmax)
-    return softargmax
-
--- src/test/python/cyclegan/test_tf_layer_tools.py
+++ src/test/python/cyclegan/test_tf_layer_tools.py
@@ -101,40 +101,5 @@ class test_tf_layer_tools(unittest.TestCase):
 
 ########################################################################################################################
 
-    def test__softargmax__1dim(self):
-        input = tf.constant( [1.0,1.1,0.9] )
-        result = lato.softargmax( input )
-        print('result2=',result)
-        self.assertEqual( 'tf.Tensor(0.99834394, shape=(), dtype=float32)', str(result) )
-
-    def test__softargmax__2dims(self):
-        input = tf.constant( [[1.0,1.1,0.9]] )
-        result = lato.softargmax( input )
-        print('result2=',result)
-        self.assertEqual( 'tf.Tensor([0.99834394], shape=(1,), dtype=float32)', str(result) )
-
-    def test__softargmax__3dims(self):
-        input = tf.constant( [[[1.0,1.1,0.9]]] )
-        result = lato.softargmax( input )
-        print('result2=',result)
-        self.assertEqual( 'tf.Tensor([[0.99834394]], shape=(1, 1), dtype=float32)', str(result) )
-
-    def test__softargmax__4dims(self):
-        input = tf.constant( [[[[1.0,1.1,0.9]]]] )
-        result = lato.softargmax( input )
-        print('result2=',result)
-        self.assertEqual( 'tf.Tensor([[[0.99834394]]], shape=(1, 1, 1), dtype=float32)', str(result) )
-
-    def test__softargmax__4dims_arrayed(self):
-        input = tf.constant( [[[[1.0,1.1,0.9],[0.9,1.0,1.1]],[[2.0,2.1,1.9],[1.9,2.0,2.1]]]] )
-        result = lato.softargmax( input )
-        print('result2=',result)
-        self.assertEqual( 'tf.Tensor(\n'
-                          '[[[0.99834394 1.9983357 ]\n'
-                          '  [0.99834394 1.9983357 ]]], shape=(1, 2, 2), dtype=float32)', str(result) )
-
-
-########################################################################################################################
-
 if __name__ == '__main__':
     unittest.main()
