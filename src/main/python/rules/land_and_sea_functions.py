bb12052f4c6be8ba5525231e41e936754d7327d3
Frederick W Haslam <fwhaslam@gmail.com>
2022-10-01 20:03:26 -0700

    Fixing land_and_sea_functions, standardized on 'goal' metrics, fixed associated tests.

--- src/main/python/rules/land_and_sea_functions.py
+++ src/main/python/rules/land_and_sea_functions.py
@@ -11,7 +11,6 @@ TERRAIN_TYPE_GOAL = tf.constant( [0.5,0.5] )
 TERRAIN_SURFACE_GOAL = 0.5
 
 PI = math.pi
-HALF = 0.5
 
 @tf.function
 def vee(x):
@@ -34,12 +33,12 @@ def peak(x):
 @tf.function
 def round_vee( x ):
     r"""-1 is at x=0.  1 is at x=+/-1"""
-    return -tf.math.sin( (x+HALF) * PI )
+    return -tf.math.sin( (x+0.5) * PI )
 
 @tf.function
 def round_peak( x ):
     r"""1 is at x=0.  -1 is at x=+/-1"""
-    return tf.math.sin( (x+HALF) * PI )
+    return tf.math.sin( (x+0.5) * PI )
 
 
 @tf.function
@@ -69,16 +68,18 @@ def terrain_type_loss(y_pred):
 
     [batch,wide,tall,deep] = y_pred.shape[0:4]
     size = tf.cast( wide * tall, tf.float32 )
-    desired = size/deep
+    # desired = size/deep
 
     counts = tf.reduce_sum( y_pred, axis=2)
     counts = tf.reduce_sum( counts, axis=1)
     # tf.print("counts=",counts)
-    type_distance = ( counts - desired ) / desired
+    type_ratio = counts / size
+    # tf.print("type_ratio=",type_ratio,'shape=',type_ratio.shape)
+    type_distance = type_ratio - TERRAIN_TYPE_GOAL
     # tf.print("type_distance=",type_distance)
 
     veed = vee(type_distance)
-    # tf.print("peaked=",veed)
+    # tf.print("veed=",veed)
     result= tf.reduce_mean( veed, axis=-1 )
     # tf.print("result=",result)
     return result
@@ -127,7 +128,7 @@ def terrain_surface_loss( y_pred ):
     #   then reduce to value [0:1] where zero means close to goal, and 1 means far from goal
     reduce_sum = tf.reduce_sum( tf.reduce_sum( diff_avg, axis=-1 ), -1 ) / size
     # tf.print('reduce_sum=',reduce_sum)
-    result = 2 * vee( reduce_sum - SURFACE_GOAL )
+    result = 2 * vee( reduce_sum - TERRAIN_SURFACE_GOAL )
     # tf.print('result=',result)
 
     # TODO: surface similarity multiplied by type vector produces type/similarity vector
