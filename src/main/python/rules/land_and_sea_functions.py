Fixed mechanics of measuring 'surface'.  Was giving incorrect values before.

src/main/python/rules/land_and_sea_functions.py
@@ -11,10 +11,23 @@ import _utilities.tf_tensor_tools as teto
 
 # number of possible output values, one value for each terrain type
 TERRAIN_TYPE_COUNT = 2
-TERRAIN_TYPE_GOAL = tf.constant( [0.5,0.5] )
-TERRAIN_SURFACE_GOAL = 0.5
-
 PI = math.pi
+
+
+
+TERRAIN_TYPE_GOAL = tf.Variable( [0.5,0.5] )
+TERRAIN_SURFACE_GOAL = tf.Variable( 0.5 )
+
+@tf.function
+def set_terrain_type_goal( ttg ):
+    global TERRAIN_TYPE_GOAL
+    TERRAIN_TYPE_GOAL.assign( ttg )
+
+@tf.function
+def set_terrain_surface_goal( tsg ):
+    global TERRAIN_SURFACE_GOAL
+    TERRAIN_SURFACE_GOAL.assign( tsg )
+
 
 @tf.function
 def vee(x):
@@ -29,10 +42,6 @@ def vee(x):
 def peak(x):
     r"""Linear inverted V shape, 0 at x=0, and -1 at x=-1|+1"""
     return tf.where( x<0, x, -x )
-    # if (x<0.5):
-    #     return 2*x
-    # else:
-    #     return 2*(1-x)
 
 @tf.function
 def round_vee( x ):
@@ -57,11 +66,16 @@ def terrain_loss( y_true, y_pred ):
 
     sm_y_pred = teto.supersoftmax( y_pred )
 
-    type_of_terrain_loss = terrain_type_loss( sm_y_pred )
+    terrain_loss = terrain_type_loss( sm_y_pred )
     certainty_loss = 0  # terrain_certainty_loss( y_pred )
     surface_loss = terrain_surface_loss( sm_y_pred )
 
-    return type_of_terrain_loss + certainty_loss + surface_loss
+    with tf.GradientTape() as t:
+        t.watch(terrain_loss)
+        # t.watch(certainty_loss)
+        t.watch(surface_loss)
+
+    return terrain_loss + certainty_loss + surface_loss
 
 
 @tf.function
@@ -109,7 +123,10 @@ def terrain_certainty_loss(y_pred):
 @tf.function
 def terrain_surface_loss( y_pred ):
     r"""'Surface' is a measure of the terrain changes accross cells.
-    It is a crude representation of the fractal level of the output."""
+    It is a crude representation of the fractal level of the output.
+    Cells are logits summing to 1.  Values are in range [0,1]
+    Same values = -1; Max difference is 90 degrees = -0
+    Cells cannot actually be opposite ( which would be +1 )"""
     # tf.print('start=',y_pred)
 
     [batch,wide,tall,deep] = y_pred.shape[0:4]
@@ -117,25 +134,25 @@ def terrain_surface_loss( y_pred ):
 
     rollh = tf.roll( y_pred, 1, 2  )
     # tf.print('rollh=',rollh)
-    diffh = -tf.keras.losses.cosine_similarity(y_pred, rollh, axis=-1 )
+    diffh = tf.keras.losses.cosine_similarity(y_pred, rollh, axis=-1 )
     # tf.print('diffh=',diffh)
 
     rollv = tf.roll( y_pred, 1, 1  )
     # tf.print('rollv=',rollv)
-    diffv = -tf.keras.losses.cosine_similarity(y_pred, rollv, axis=-1 )
+    diffv = tf.keras.losses.cosine_similarity(y_pred, rollv, axis=-1 )
     # tf.print('diffv=',diffv)
 
     diff_avg= ( diffh + diffv ) / 2.
     # tf.print('diff_avg=',diff_avg)
 
-    # add together a count of 'similar', so range[0:size]
-    #   then reduce to value [0:1] where zero means close to goal, and 1 means far from goal
-    reduce_sum = tf.reduce_sum( tf.reduce_sum( diff_avg, axis=-1 ), -1 ) / size
-    # tf.print('reduce_sum=',reduce_sum)
-    result = 2 * vee( reduce_sum - TERRAIN_SURFACE_GOAL )
+    same_count = tf.reduce_sum( tf.reduce_sum( diff_avg, axis=-1 ), -1 ) / size
+    # tf.print('same_count(negative)=',same_count)
+    change_count = 1. + same_count
+    # tf.print('change_sum=',change_count)
+    # tf.print('TERRAIN_SURFACE_GOAL=',TERRAIN_SURFACE_GOAL)
+    result = 2 * vee( change_count - TERRAIN_SURFACE_GOAL )
     # tf.print('result=',result)
 
     # TODO: surface similarity multiplied by type vector produces type/similarity vector
     #       and goal is expressed as vector of similarity by type
     return result
-

src/test/python/rules/test_land_and_sea_functions.py
@@ -110,7 +110,6 @@ class test_land_and_sea(unittest.TestCase):
         self.assertEqual('(3,)',str(result.shape))
         self.assertEqual('<dtype: \'float32\'>',str(result.dtype))
 
-        value = tensor_to_value( result )
         self.assertAlmostEqual( 0., result[0], places=6 )
         self.assertAlmostEqual( 0., result[1], places=6 )
         self.assertAlmostEqual( 0., result[2], places=6 )
@@ -118,7 +117,7 @@ class test_land_and_sea(unittest.TestCase):
     def test__terrain_loss__mostly_bad(self):
         # test terrain_loss
 
-        # logits to [[1,0],[1,0]]
+        # logits to [[1,1],[1,1]]
         element = [[[0.4,0.6],
                     [0.4,0.6]],
                    [[0.4,0.6],
@@ -132,13 +131,17 @@ class test_land_and_sea(unittest.TestCase):
         # tf.print("RESULT=",result)
 
         # assertions
-        # self.assertTrue( np.array_equal( [0,0,0], tf.get_static_value(result) ) )
-        self.assertEqual( "tf.Tensor([1.9 1.9 1.9], shape=(3,), dtype=float32)", str(result) )
+        self.assertEqual('(3,)',str(result.shape))
+        self.assertEqual('<dtype: \'float32\'>',str(result.dtype))
+
+        self.assertAlmostEqual( 1.4999973, result[0], places=6 )
+        self.assertAlmostEqual( 1.4999973, result[1], places=6 )
+        self.assertAlmostEqual( 1.4999973, result[2], places=6 )
 
     def test__terrain_loss__types_failure(self):
         # test terrain_loss
 
-        # logits to [[?,0],[1,1]]
+        # logits to [[1,1],[1,1]]
         element = [[[0.,1.],
                     [0.,1.]],
                    [[0.,1.],
@@ -155,7 +158,6 @@ class test_land_and_sea(unittest.TestCase):
         self.assertEqual('(3,)',str(result.shape))
         self.assertEqual('<dtype: \'float32\'>',str(result.dtype))
 
-        value = tensor_to_value( result )
         self.assertAlmostEqual( 1.5, result[0], places=6 )
         self.assertAlmostEqual( 1.5, result[1], places=6 )
         self.assertAlmostEqual( 1.5, result[2], places=6 )
@@ -423,9 +425,35 @@ class test_land_and_sea(unittest.TestCase):
         # assertions
         self.assertEqual('(1,)',str(result.shape))
         self.assertEqual('<dtype: \'float32\'>',str(result.dtype))
-
-        value = tensor_to_value( result )
         self.assertAlmostEqual( 0.241292, result[0], places=6 )
+
+
+    def test__terrain_surface_loss__at_third(self):
+
+        print("lnz.TERRAIN_SURFACE_GOAL=",lnz.TERRAIN_SURFACE_GOAL)
+        old_value = tensor_to_value( lnz.TERRAIN_SURFACE_GOAL )
+        lnz.set_terrain_surface_goal( 0.3 )
+
+        try:
+            # logits to [[1,0],[1,1]]
+            element = [ [[0.,1.],[0.,1.],[0.,1.]],
+                        [[1.,0.],[1.,0.],[1.,0.],],
+                        [[1.,0.],[1.,0.],[1.,0.],] ]
+            # batch_size=1, wide/tall=2x2, types=2
+            input = tf.constant( [element,]  )
+
+            # invocation
+            result = lnz.terrain_surface_loss( input )
+            # print("RESULT=",result)
+
+            # assertions
+            self.assertEqual('(1,)',str(result.shape))
+            self.assertEqual('<dtype: \'float32\'>',str(result.dtype))
+            self.assertAlmostEqual( 0.066667, result[0], places=6 )
+        finally:
+            lnz.set_terrain_surface_goal( old_value )
+            print("lnz.TERRAIN_SURFACE_GOAL=",lnz.TERRAIN_SURFACE_GOAL)
+
 
     def test__terrain_surface_loss__has_gradient(self):
         self.assertTrue( sft.loss_has_gradient( lnz.terrain_surface_loss, output_shape=(2,2,2) ) )